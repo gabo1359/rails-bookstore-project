e7065553b3df5ca1a86d7787290b470042fd49c1
Diana Pinto <diana.ppm18@gmail.com>
2022-01-26 11:24:09 -0500

    books controller listo

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'simple_form'
+gem 'faker', '~> 2.19'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# Como usuario puedo ver los productos disponibles
+   get "books", to: "books#index"
+# Como usuario puedo ver los detalles del libro
+  get "books/:id", to: "books#show", as: :book
+# como usuario puedo poner un libro en alquiler
+  get "books/new", to: "books#new"
+  post "books", to: "books#create"
+# como usuario puedo editar los detalles de mi libro.
+  get "books/:id/edit", to: "books#edit"
+  patch "books/:id", to: "books#update"
+# como usuario puedo eliminarlo,
+  delete "books/:id", to: "books#destroy"
 end
