2c076da030b5f012138a4b1ebed8c631a12bafc9
Gabriel <gabs1995@hotmail.com>
2022-01-26 12:56:09 -0500

    order changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,9 @@ class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
   def index
     @books = Book.all
-
   end
-  def show
 
+  def show
   end
 
   def new
