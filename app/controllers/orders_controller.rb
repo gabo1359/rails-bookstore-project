cbad4663923e1b165813aa9b9ccb7f5380aa3a4e
Gabriel <gabs1995@hotmail.com>
2022-01-26 11:20:43 -0500

    order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+  end
+    
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user
+    @order.book_id = params[:book_id]
+    
+    if @order.save
+      redirect_to orders_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:start_date, :end_date)
+  end
 end
