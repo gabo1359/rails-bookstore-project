549feeb3654caf7785a650e8bc652c8d8edded16
luis <hilario.huaman.6@gmail.com>
2022-01-28 15:54:06 -0500

    proyecto finalizado

--- db/schema.rb
+++ db/schema.rb
@@ -81,9 +81,9 @@ ActiveRecord::Schema.define(version: 2022_01_28_201028) do
     t.string "address"
     t.string "phone"
     t.string "username"
+    t.boolean "admin"
     t.float "latitude"
     t.float "longitude"
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
